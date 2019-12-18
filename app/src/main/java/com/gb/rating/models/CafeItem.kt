ed026bfb1d34ad73c5df4c8f1e86ef8a67ed7de5
Jvitaliy82 <jvit82@gmail.com>
2019-12-18 23:23:51 +0700

    сделан список с карточками

---
+++ app/src/main/java/com/gb/rating/models/CafeItem.kt
@@ -0,0 +1,9 @@
+package com.gb.rating.models
+
+class CafeItem(
+    var img: Int,
+    var name: String = "",
+    var desc: String = "",
+    var rating: Int,
+    var distance: Double
+    )
--- app/src/main/java/com/gb/rating/ui/list/ListFragment.kt
+++ app/src/main/java/com/gb/rating/ui/list/ListFragment.kt
@@ -4,30 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gb.rating.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
-    private lateinit var listViewModel: ListViewModel
+    private val listViewModel by lazy { ViewModelProviders.of(this).get(ListViewModel::class.java) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        listViewModel =
-            ViewModelProviders.of(this).get(ListViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_list, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        listViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
-    //test for commit
+    override fun onStart() {
+        super.onStart()
+        //инициализируем адаптер и присваеваем его списку
+        val adapter = ListAdapter()
+        cafeListRecycler_FragmentList?.layoutManager = LinearLayoutManager(activity)
+        cafeListRecycler_FragmentList?.adapter = adapter
+        listViewModel.getListCafe().observe(this, Observer {
+            it?.let { adapter.refreshList(it) }
+        })
+    }
 }
