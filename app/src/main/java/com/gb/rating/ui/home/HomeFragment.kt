72147b0881853b21f905eabd8a889875a6e71424
Kharin1986 <harin_a_a@mail.ru>
2020-01-18 23:48:20 +0500

    Added cards for Home Added Navigation from Home (and changed fragment) Renamed Offline Fragment (to Settings) Bottom menu consists of Home, Отзыв, Список, Поиск, Настройки. Locked screen rotation (in AndroidManifest)

--- app/src/main/java/com/gb/rating/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gb/rating/ui/home/HomeFragment.kt
@@ -1,16 +1,23 @@
 package com.gb.rating.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+
 import androidx.lifecycle.ViewModelProviders
+import android.widget.ImageButton
+import androidx.fragment.app.ListFragment
 import com.gb.rating.R
+import com.gb.rating.ui.search.SearchFragment
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
+
 
 class HomeFragment : Fragment() {
+
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -22,10 +29,6 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
--- app/src/main/java/com/gb/rating/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/gb/rating/ui/settings/SettingsFragment.kt
@@ -1,4 +1,4 @@
-package com.gb.rating.ui.offline
+package com.gb.rating.ui.settings
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,23 +9,22 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.gb.rating.R
-import com.gb.rating.ui.offline.OfflineViewModel
 
 
-class OfflineFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private lateinit var offlineViewModel: OfflineViewModel
+    private lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        offlineViewModel =
-            ViewModelProviders.of(this).get(OfflineViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_offline, container, false)
-        val textView: TextView = root.findViewById(R.id.text_offline)
-        offlineViewModel.text.observe(this, Observer {
+        settingsViewModel =
+            ViewModelProviders.of(this).get(SettingsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+        val textView: TextView = root.findViewById(R.id.text_settings)
+        settingsViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
