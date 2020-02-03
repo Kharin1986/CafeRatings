7d7ee4076e4a3bf943be775d778066d5507b9e96
Michael <3yebmb@gmail.com>
2020-02-03 17:23:40 +0500

    add auto scaling on the main page

--- app/src/main/java/com/gb/rating/ui/home/HomeFragment.kt
+++ app/src/main/java/com/gb/rating/ui/home/HomeFragment.kt
@@ -1,23 +1,18 @@
 package com.gb.rating.ui.home
 
 import android.os.Bundle
-import android.util.Log
+import android.util.DisplayMetrics
+import android.view.Display
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
 import androidx.lifecycle.ViewModelProviders
-import android.widget.ImageButton
-import androidx.fragment.app.ListFragment
 import com.gb.rating.R
-import com.gb.rating.ui.search.SearchFragment
 import kotlinx.android.synthetic.main.fragment_home.*
-import kotlinx.android.synthetic.main.fragment_home.view.*
-
+import kotlin.math.roundToInt
 
 class HomeFragment : Fragment() {
-
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -30,5 +25,38 @@ class HomeFragment : Fragment() {
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val w = activity!!.windowManager
+        val d: Display = w.defaultDisplay
+        val metrics = DisplayMetrics()
+        d.getMetrics(metrics)
+        val hScale = metrics.heightPixels.toFloat()/metrics.widthPixels.toFloat()
+
+        cl_rest.layoutParams.height = (226*hScale*1.95).roundToInt()
+        cl_rest.layoutParams.width = (158*3.0).roundToInt()
+        cl_rest.requestLayout()
+
+        cl_bar.layoutParams.height = (111*hScale*1.95).roundToInt()
+        cl_bar.layoutParams.width = (158*3.0).roundToInt()
+        cl_bar.requestLayout()
+
+        cl_fav.layoutParams.height = (71*hScale*1.95).roundToInt()
+        cl_fav.layoutParams.width = (158*3.0).roundToInt()
+        cl_fav.requestLayout()
+
+        cl_top.layoutParams.height = (71*hScale*1.95).roundToInt()
+        cl_top.layoutParams.width = (158*3.0).roundToInt()
+        cl_top.requestLayout()
+
+        cl_cafe.layoutParams.height = (142*hScale*1.95).roundToInt()
+        cl_cafe.layoutParams.width = (158*3.0).roundToInt()
+        cl_cafe.requestLayout()
+
+        cl_fastfood.layoutParams.height = (194*hScale*1.95).roundToInt()
+        cl_fastfood.layoutParams.width = (158*3.0).roundToInt()
+        cl_fastfood.requestLayout()
     }
 }
