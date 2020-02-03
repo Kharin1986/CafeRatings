229ee04db33784520e018374b5aabb4e90eff27c
Jvitaliy82 <jvit82@gmail.com>
2020-02-03 14:38:55 +0700

    переход на GoogleMaps

--- app/src/main/java/com/gb/rating/ui/search/SearchFragment.java
+++ app/src/main/java/com/gb/rating/ui/search/SearchFragment.java
@@ -3,68 +3,54 @@ package com.gb.rating.ui.search;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.gb.rating.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
 
-public class MapFragment extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPoiClickListener{
+public class SearchFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnPoiClickListener {
 
+    private View view;
 
     private final String TAG = "myLog";
     private GoogleMap mMap;
-    public Marker marker;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_search, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
+        return view;
     }
 
     private void init() {
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-            @Override
-            public void onMapClick(LatLng latLng) {
-                Log.d(TAG, "onMapClick: " + latLng.latitude + ", " + latLng.longitude);
-            }
-        });
+        mMap.setOnMapClickListener(latLng -> Log.d(TAG, "onMapClick: " + latLng.latitude + ", " + latLng.longitude));
 
-        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-            @Override
-            public void onMapLongClick(LatLng latLng) {
-                Log.d(TAG, "onMapLongClick: " + latLng.latitude + ", " + latLng.longitude);
-            }
-        });
-
-        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
-            @Override
-            public void onCameraChange(CameraPosition cameraPosition) {
-                Log.d(TAG, "onCameraChange: " + cameraPosition.target.latitude + ", " + cameraPosition.target.longitude);
-            }
-        });
+        mMap.setOnMapLongClickListener(latLng -> Log.d(TAG, "onMapLongClick: " + latLng.latitude + ", " + latLng.longitude));
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng moscow = new LatLng(55.6902907, 37.6850383);
+        mMap.addMarker(new MarkerOptions().position(moscow).title("Marker in Moscow"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(moscow));
         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.setOnPoiClickListener(this);
@@ -73,11 +59,12 @@ public class MapFragment extends FragmentActivity implements OnMapReadyCallback,
 
     @Override
     public void onPoiClick(PointOfInterest poi) {
-        Toast.makeText(getApplicationContext(), "Clicked: " +
+        Toast.makeText(getContext(), "Clicked: " +
                         poi.name + "\nPlace ID:" + poi.placeId +
                         "\nLatitude:" + poi.latLng.latitude +
                         " Longitude:" + poi.latLng.longitude,
                 Toast.LENGTH_SHORT).show();
     }
+
 
 }
